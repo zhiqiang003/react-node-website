a9426e344c6cd08352f7d7e7fbc4d1dec6223292
yanweiqing <yanweiqing@youzan.com>
2016-07-11 22:02:56 +0800

    添加前端文件

---
+++ app/core/Layout.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import createApp from '../createApp.js';
+
+export default class Layout extends Component {
+    render() {
+        return <div>123</div>;
+    }
+}
+
+createApp(Layout);
